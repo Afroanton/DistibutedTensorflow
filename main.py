9416c6e620aacca5575b90ef960154ca50311e97
Afroanton <47089211+Afroanton@users.noreply.github.com>
2021-05-03 20:33:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-
 import tensorflow as tf
 import tfConfig as con
 import model
 
-worker_index = 0
-tf_config = con.config(worker_index)
+
+#tf_config = con.config()
 strategy = tf.distribute.MultiWorkerMirroredStrategy()
-dataset = model.get_dataset()
+global_batch = 64 * 4
+dataset = model.get_dataset(global_batch)
 
 with strategy.scope():
     worker_model = model.build_and_compile_cnn_model()
 
-worker_model.fit(dataset[0][0], dataset[0][1], epochs=3)
+worker_model.fit(dataset, epochs=3, steps_per_epoch=70)
