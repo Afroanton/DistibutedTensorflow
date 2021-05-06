80f649db36957e2d831e952317fc13c890a30e5f
Afroanton <anton_zam@live.se>
2021-05-06 18:17:46 +0200

    fixed tfConfig for 4 workers

--- tfConfig.py
+++ tfConfig.py
@@ -2,13 +2,13 @@ import json
 import os
 
 
-def config(worker_index):
+def config():
     os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
     os.environ.pop('TF_CONFIG', None)
     os.environ['TF_CONFIG'] = json.dumps({
         "cluster": {
-            "chief": ["ip:12345"],
-            "worker": ["ip:23456"]
+            "chief": ["192.168.1.115:12345"],
+            "worker": ["192.168.1.187:23456", "192.168.1.122:34567", "192.168.1.116:45678"]
         },
         "task": {"type": "chief", "index": 0}
     })
